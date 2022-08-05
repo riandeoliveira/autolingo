d4a706c5d4f7da7bf91e6cc79a847842de430b30
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-08-05 10:52:34 -0300

    feat: add puppeteer

--- TODO.md
+++ TODO.md
@@ -1,11 +1,7 @@
 # TODO List
 
-- [ ] Refatorar código
-- [ ] Fazer o bot percorrer a última etapa de escolher as alternativas
-- [ ] Integrar com Puppeteer
-
-- 01/08 (09:00)
-- 07/08 (08:00)
-- 13/08 (07:00)
-- 19/08 (06:00)
-- 25/08 (05:00)
+- [x] Utilizar dotenv.
+- [ ] Fazer commit.
+- [ ] Fechar o navegador ao terminar.
+- [ ] Adicionar mensagens durante o processo.
+- [ ] Adicionar tratamento de erros.
--- index.js
+++ index.js
@@ -1,36 +1,83 @@
-const interval = setInterval(() => {
-  const continueButton = document.querySelector("._3HhhB");
-  const checkBoxButtons = Array.from(document.querySelectorAll("._21Icd"));
-  const optionsButtons = Array.from(document.querySelectorAll("._1rl91"));
-  const buttonsList = Array.from(document.querySelectorAll(".WOZnx"));
-  const leftButtons = buttonsList.slice(0, 5);
-  const rightButtons = buttonsList.slice(5, 10);
+const dotenv = require("dotenv");
+const puppeteer = require("puppeteer");
 
-  continueButton.click();
+dotenv.config();
 
-  checkBoxButtons.map((button) => button.click());
-  optionsButtons.map((button) => button.click());
+const pages = {
+  auth: "https://www.duolingo.com/?isLoggingIn=true",
+  story: "https://www.duolingo.com/stories/en-pt-a-date?mode=read",
+};
 
-  if (buttonsList.length <= 5) {
-    buttonsList.map((button) => button.click());
-  } else {
-    clearInterval(interval);
+const selectors = {
+  email: "input[type='text']",
+  password: "input[type='password']",
+  submit: "button[type='submit']",
+};
 
-    let leftIndex = 0;
-    let rightIndex = 0;
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ["--start-maximized"],
+    defaultViewport: null,
+  });
 
-    setInterval(() => {
-      leftButtons[leftIndex].click();
-      rightButtons[rightIndex].click();
+  const authPage = await browser.newPage();
+  await authPage.goto(pages.auth);
 
-      if (rightIndex >= 4) {
-        leftIndex++;
-        rightIndex = 0;
-      } else rightIndex++;
+  await authPage.waitForSelector(".WOZnx");
+  await authPage.click(".WOZnx");
 
+  await authPage.waitForTimeout(5000);
+
+  await authPage.type(selectors.email, process.env.EMAIL);
+  await authPage.type(selectors.password, process.env.PASSWORD);
+  await authPage.click(selectors.submit);
+
+  await authPage.waitForTimeout(5000);
+
+  const storyPage = await browser.newPage();
+  await storyPage.goto(pages.story);
+
+  await storyPage.evaluate(() => {
+    const interval = setInterval(() => {
       const continueButton = document.querySelector("._3HhhB");
+      const checkBoxButtons = Array.from(document.querySelectorAll("._21Icd"));
+      const optionsButtons = Array.from(document.querySelectorAll("._1rl91"));
+      const buttonsList = Array.from(document.querySelectorAll(".WOZnx"));
+      const leftButtons = buttonsList.slice(0, 5);
+      const rightButtons = buttonsList.slice(5, 10);
 
       continueButton.click();
-    }, 1000);
-  }
-}, 0);
+
+      checkBoxButtons.map((button) => button.click());
+      optionsButtons.map((button) => button.click());
+
+      if (buttonsList.length <= 5) {
+        buttonsList.map((button) => button.click());
+      } else {
+        clearInterval(interval);
+
+        let leftIndex = 0;
+        let rightIndex = 0;
+
+        setInterval(() => {
+          leftButtons[leftIndex].click();
+          rightButtons[rightIndex].click();
+
+          if (rightIndex >= 4) {
+            leftIndex++;
+            rightIndex = 0;
+          } else rightIndex++;
+
+          const continueButton = document.querySelector("._3HhhB");
+
+          continueButton.click();
+        }, 1000);
+      }
+    }, 0);
+  });
+
+  await storyPage.waitForTimeout(100000);
+
+  await browser.close();
+})();
